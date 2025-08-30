2bfacc54b6ffdf5f4909032b01cebcd68a6a29f6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-08-30 16:06:40 +0200

    Revert "v3.01"
    This reverts commit 9f61d5e402d6797518ba13909a4d44203f89f841.

---
+++ Source/WDYS/DefAlterer.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace WDYS
+{
+    [StaticConstructorOnStartup]
+    internal class DefAlterer
+    {
+        static DefAlterer()
+        {
+            DoAlteration();
+        }
+
+        public static void DoAlteration()
+        {
+            if (WDYS_Mod.settings.activateGlobalStockTab)
+                DefDatabase<MainButtonDef>.GetNamed("WFS_TradeStock").buttonVisible = true;
+            else
+                DefDatabase<MainButtonDef>.GetNamed("WFS_TradeStock").buttonVisible = false;
+        }
+    }
+}
